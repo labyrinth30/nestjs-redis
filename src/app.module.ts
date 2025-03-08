c2b07c4df00fd14155d78e4113b127ea762c813c
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-08 22:19:04 +0900

    RDS, ElastiCache 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,26 +5,52 @@ import { Board } from './board.entity';
 import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-ioredis';
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'my-db',
-      port: 3306,
-      username: 'user1',
-      password: 'skhuA+4.5',
-      database: 'mydb',
-      autoLoadEntities: true,
-      synchronize: true,
-      logging: true,
+    TypeOrmModule.forFeature([Board]),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [Board],
+        logging: true,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().valid('postgres').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        CACHE_HOST: Joi.string().required(),
+        CACHE_PORT: Joi.number().required(),
+        CACHE_TTL: Joi.number().required(),
+      }),
     }),
     TypeOrmModule.forFeature([Board]),
-    CacheModule.register({
-      store: redisStore,
-      host: 'my-cache-server',
-      port: 6379,
-      ttl: 10000,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get<string>('CACHE_HOST'),
+        port: configService.get<number>('CACHE_PORT'),
+        ttl: configService.get<number>('CACHE_TTL'),
+      }),
     }),
   ],
   controllers: [BoardController],
